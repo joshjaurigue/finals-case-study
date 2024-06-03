328b5b3d41e9e309d5c8c1c906a4dda94661369b
Jeremy Joshua R. Jaurigue <jauriguejeremyjoshua02@gmail.com>
2024-06-04 01:29:21 +0800

    database
    migrated database

---
+++ laravel/database/migrations/2024_06_03_154726_create_medical_records_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medical_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
+            $table->foreignId('doctor_id')->constrained('doctors')->onDelete('cascade');
+            $table->enum('type', ['diagnosis', 'prescription', 'treatment'])->nullable();
+            $table->text('record')->nullable();
+            $table->dateTime('record_date')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medical_records');
+    }
+};
